01f8651231086bdd3975fdb90909f28d1d43a89b
Wiktor Rzymski <u138_wikrzy_waw@technischools.com>
2025-05-13 10:41:10 +0200

    Reapply "logic + refactor"
    This reverts commit 5fd4931955714b40283fe2d7ff5fa0da355f325d.

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+from repository.user_repository import UserRepository
+from service.user_service import UserService
+from datetime import datetime
+
+app = Flask(__name__)
+repo = UserRepository()
+service = UserService(repo)
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    now = datetime.now().year
+    users = [u.to_dict(now) for u in service.get_all_users()]
+    return jsonify(users), 200
+
+@app.route("/users/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    user = service.get_user(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify(user.to_dict(datetime.now().year)), 200
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.json
+    try:
+        user = service.create_user(data)
+        return jsonify(user.to_dict(datetime.now().year)), 201
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route("/users/<int:user_id>", methods=["PATCH"])
+def update_user(user_id):
+    data = request.json
+    try:
+        user = service.update_user(user_id, data)
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+        return jsonify(user.to_dict(datetime.now().year)), 200
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route("/users/<int:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    user = service.delete_user(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    return '', 204
+
+if __name__ == "__main__":
+    app.run(debug=True)
